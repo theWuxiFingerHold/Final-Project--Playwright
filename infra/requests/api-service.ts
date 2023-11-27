64bd514d3df7753d09cf9af2fab147cd0a4de662
AintSmurf <monkeydyasuo1@gmail.com>
2023-11-27 19:44:09 +0200

    api

--- infra/requests/api-service.ts
+++ infra/requests/api-service.ts
@@ -6,7 +6,6 @@ export class ApiService {
     post = async <T extends object>(url: string, data: T): Promise<any> => {
         const context = await request.newContext();
         const response = await context.post(`${url}`, { data: data })
-        console.log(await response.body())
         return await response.json()
     }
 
--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,6 +1,4 @@
 import { test } from "@playwright/test"
-import { ApiService } from "../infra/requests/api-service"
-import { createLogin } from "../logic/requests/interface/login-interface-req"
 import { HomePage } from "../logic/pages/home-page"
 import { HttpHelper } from "../logic/requests/interface/http-helper"
 
@@ -8,7 +6,6 @@ test.describe('Testing Terminal-X', async () => {
     test('dummy', async ({ page }) => {
         const home = new HomePage(page)
         await home.goto()
-        const api = new ApiService()
         const hp = new HttpHelper()
         await hp.login()
         await page.reload()
